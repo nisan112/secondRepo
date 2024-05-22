037cae1c74e10c9cd98b0b8c3e9c8de19a4867f1
Nishan Shrestha <nisanstha355@gmail.com>
2024-05-21 23:56:19 -0400

    changed

--- secondClass.java
+++ secondClass.java
@@ -4,7 +4,7 @@ public class secondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World this is second commit");
+		System.out.println("Hello World this is second commit updated and changed");
 
 	}
 
