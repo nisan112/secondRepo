4affbd8922de77933445e5d72862f1829f35f693
Nishan Shrestha <nisanstha355@gmail.com>
2024-05-21 23:42:07 -0400

    second commit

---
+++ secondClass.java
@@ -0,0 +1,11 @@
+package secondClass;
+
+public class secondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this is second commit");
+
+	}
+
+}
